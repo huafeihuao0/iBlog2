b34ee21872ac82091e851d6d1bf0a2d0992564a8
liruifeng <liruifeng@secboot.com>
2017-10-30 18:22:42 +0800

    重构app.js，重命名路由

--- app.js
+++ app.js
@@ -4,69 +4,108 @@ var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var bodyParser = require('body-parser');
-var route = require('./routes/index');
-var blog = require('./routes/blog');
-var misc = require('./routes/misc');
-var auth = require('./routes/auth');
-var admin = require('./routes/admin');
-var locale = require('./routes/locale');
-var ue = require('./routes/ue');
 var logger = require('./utility/logger');
 var passport = require('passport');
 var i18n = require('./models/i18n');
 var saker = require('saker');
-saker.config({
-    defaultLayout: './shared/layout.html',
-    partialViewDir: './views/shared/'
-});
+
+var bodyParser = require('body-parser');
+var indexCtrler = require('./routes/index');
+var blogCtrler = require('./routes/blog');
+var miscCtrler = require('./routes/misc');
+var authCtrler = require('./routes/auth');
+var adminCtrler = require('./routes/admin');
+var localeCtrler = require('./routes/locale');
+var ueCtrler = require('./routes/ue');
+
 var app = express();
 
-// view engine setup
-app.engine('html', saker.renderView);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+(function init()
+{
+    /*设置模板引擎*/
+    mSetupViewEngine();
+    /*使用中间件*/
+    mUseMids();
+})();
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-// i18n init parses req for language headers, cookies, etc.
-app.use(i18n.init);
+/**
+ * 设置模板引擎
+ **/
+function mSetupViewEngine()
+{
+    saker.config({
+        defaultLayout: './shared/layout.html',
+        partialViewDir: './views/shared/'
+    });
+    app.engine('html', saker.renderView);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'html');
+}
 
-app.use(session({
-    secret: 'iblog-exp-session',
-    cookie: {
-        maxAge: 24 * 60 * 60 * 1000
-    },
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * 使用中间件
+ **/
+function mUseMids()
+{
+    app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));//服务器图标
+    app.use(morgan('dev'));//日志
+    app.use(bodyParser.json());//post内容体解析
+    app.use(bodyParser.urlencoded({extended: false}));//url参数解析
+    app.use(cookieParser());//cookie解析
+    app.use(i18n.init);//国际化
+    /*使用会话中间件*/
+    mUseSessMid();
+    app.use(express.static(path.join(__dirname, 'public')));//静态资源
+    app.use(passport.initialize());//认证解析
+    app.use(passport.session());
 
-app.use(passport.initialize());
-app.use(passport.session());
+    app.use('/', indexCtrler);
+    app.use('/', localeCtrler);
+    app.use('/', miscCtrler);
+    app.use('/', authCtrler);
+    app.use('/blog', blogCtrler);
+    app.use('/admin', require('connect-ensure-login').ensureLoggedIn('/login'), adminCtrler);
+    app.use('/ue/controller', ueCtrler);
 
-app.use('/', route);
-app.use('/', locale);
-app.use('/', misc);
-app.use('/', auth);
-app.use('/blog', blog);
-app.use('/admin', require('connect-ensure-login').ensureLoggedIn('/login'), admin);
-app.use('/ue/controller', ue);
+    /*404处理*/
+    app.use(m404Mid);
+    /*500错误*/
+    app.use(m500Mid);
+}
 
+/**
+* 使用会话中间件
+**/
+function mUseSessMid()
+{
+    var sessOpts =//
+        {
+            secret: 'iblog-exp-session',
+            cookie://
+                {
+                    maxAge: 24 * 60 * 60 * 1000
+                },
+            resave: false,
+            saveUninitialized: false
+        }
+    app.use(session(sessOpts));
+}
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+/**
+* 404处理中间件
+**/
+function m404Mid(req, res, next)
+{
     var err = new Error();
     err.status = 404;
     next(err);
-});
+}
 
-// error handlers
-app.use(function (err, req, res, next) {
+/**
+* 500处理中间件
+**/
+function m500Mid(err, req, res, next)
+{
     var code = err.status || 500,
         message = code === 404 ? res.__('error.404_1') : res.__('error.404_2');
     res.status(code);
@@ -75,6 +114,6 @@ app.use(function (err, req, res, next) {
         code: code,
         message: message
     });
-});
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,46 @@ var async = require('async');
 var category = require('../proxy/category');
 var tool = require('../utility/tool');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res, next)
+{
     async.parallel([
         //获取配置
-        function (cb) {
-            tool.getConfig(path.join(__dirname, '../config/settings.json'), function (err, settings) {
-                if (err) {
+        function (cb)
+        {
+            tool.getConfig(path.join(__dirname, '../config/settings.json'), function (err, settings)
+            {
+                if (err)
+                {
                     cb(err);
-                } else {
+                } else
+                {
                     cb(null, settings);
                 }
             });
         },
         //获取分类
-        function (cb) {
-            category.getAll(function (err, categories) {
-                if (err) {
+        function (cb)
+        {
+            category.getAll(function (err, categories)
+            {
+                if (err)
+                {
                     cb(err);
-                } else {
+                } else
+                {
                     cb(null, categories);
                 }
             });
         }
-    ], function (err, results) {
+    ], function (err, results)
+    {
         var settings,
             categories;
-        if (err) {
+        if (err)
+        {
             next(err);
-        } else {
+        } else
+        {
             settings = results[0];
             categories = results[1];
             res.render('blog/index', {
