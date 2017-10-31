15808dd168d7f6c8a6e6bfe011e10bd5c879e379
liruifeng <liruifeng@secboot.com>
2017-10-31 11:35:45 +0800

    重构首页控制器

--- routes/index.js
+++ routes/index.js
@@ -7,55 +7,81 @@ var tool = require('../utility/tool');
 
 router.get('/', function (req, res, next)
 {
-    async.parallel([
-        //获取配置
-        function (cb)
-        {
-            tool.getConfig(path.join(__dirname, '../config/settings.json'), function (err, settings)
-            {
-                if (err)
-                {
-                    cb(err);
-                } else
-                {
-                    cb(null, settings);
-                }
-            });
-        },
-        //获取分类
-        function (cb)
-        {
-            category.getAll(function (err, categories)
-            {
-                if (err)
-                {
-                    cb(err);
-                } else
-                {
-                    cb(null, categories);
-                }
-            });
-        }
-    ], function (err, results)
+    var paraTasks =//并行任务
+        [
+            mGetConfigCreator(),//获取配置生成器
+            mGetCategoriesCreator()//获取分类任务生成器
+        ]
+    async.parallel(paraTasks, function (err, results)
     {
-        var settings,
-            categories;
         if (err)
         {
             next(err);
         } else
         {
-            settings = results[0];
-            categories = results[1];
-            res.render('blog/index', {
-                cateData: categories,
-                config: settings,
-                title: settings['SiteName'],
-                currentCate: '',
-                isRoot: true
-            });
+            /*渲染首页*/
+            mRendIndex(results)
         }
     });
 });
+
+/**
+* 渲染首页
+ * @param paraResults {Array} 并行任务结果集
+**/
+function mRendIndex(paraResults)
+{
+    var settings= paraResults[0],
+        categories= paraResults[1];
+    res.render('blog/index', {
+        cateData: categories,
+        config: settings,
+        title: settings['SiteName'],
+        currentCate: '',
+        isRoot: true
+    });
+}
+
+/**
+ * 获取配置生成器
+ **/
+function mGetConfigCreator()
+{
+    //获取配置
+    return function mGetConfig(cb)
+    {
+        tool.getConfig(path.join(__dirname, '../config/settings.json'), function (err, settings)
+        {
+            if (err)
+            {
+                cb(err);
+            } else
+            {
+                cb(null, settings);
+            }
+        });
+    };
+}
+
+/**
+* 获取分类任务生成器
+**/
+function mGetCategoriesCreator()
+{
+    //获取分类
+    return function mGetCategories(cb)
+    {
+        category.getAll(function (err, categories)
+        {
+            if (err)
+            {
+                cb(err);
+            } else
+            {
+                cb(null, categories);
+            }
+        });
+    }
+}
 
 module.exports = router;
