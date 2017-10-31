129d21890eb90821be6e512dfa88329453aecff0
liruifeng <liruifeng@secboot.com>
2017-10-31 10:18:10 +0800

    重构日志代理工具

--- proxy/log.js
+++ proxy/log.js
@@ -5,23 +5,14 @@ var logModel = require('../models/log').LogModel;
  * @param params 参数对象
  * @param callback 回调函数
  */
-exports.getAll = function (params, callback) {
-    var page = parseInt(params.pageIndex) || 1;
-    var size = parseInt(params.pageSize) || 10;
-    page = page > 0 ? page : 1;
-    var options = {};
-    options.skip = (page - 1) * size;
-    options.limit = size;
-    switch (params.sortName) {
-        case 'level':
-            options.sort = params.sortOrder === 'desc' ? '-level -timestamp' : 'level timestamp';
-            break;
-        default:
-            options.sort = params.sortOrder === 'desc' ? '-timestamp' : 'timestamp';
-            break;
-    }
-    logModel.find({}, {}, options, function (err, logs) {
-        if (err) {
+function getAll(params, callback)
+{
+    /*拼接查询选项*/
+    var options = mConcatQueryOpts(params);
+    logModel.find({}, {}, options, function (err, logs)
+    {
+        if (err)
+        {
             return callback(err);
         }
         return callback(null, logs);
@@ -29,16 +20,46 @@ exports.getAll = function (params, callback) {
 };
 
 /**
+ * 拼接查询选项
+ * @param params 参数对象
+ **/
+function mConcatQueryOpts(params)
+{
+    var page = parseInt(params.pageIndex) || 1;
+    var size = parseInt(params.pageSize) || 10;
+    page = page > 0 ? page : 1;
+    var options = {};
+    options.skip = (page - 1) * size;
+    options.limit = size;
+    switch (params.sortName)
+    {
+        case 'level':
+            options.sort = params.sortOrder === 'desc' ? '-level -timestamp' : 'level timestamp';
+            break;
+        default:
+            options.sort = params.sortOrder === 'desc' ? '-timestamp' : 'timestamp';
+            break;
+    }
+    return options;
+}
+
+/**
  * 获取日志数
  * @param params 参数对象
  * @param callback 回调函数
  */
-exports.getAllCount = function (params, callback) {
-    logModel.count(function (err, count) {
-        if (err) {
+function getAllCount(params, callback)
+{
+    logModel.count(function (err, count)
+    {
+        if (err)
+        {
             return callback(err);
         }
         return callback(null, count);
     });
 };
 
+
+exports.getAll = getAll; //获取所有日志
+exports.getAllCount = getAllCount; //获取日志数
