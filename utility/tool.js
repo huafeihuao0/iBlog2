6ccc83102d65dcc9749b2c631eccad1210763476
liruifeng <liruifeng@secboot.com>
2017-10-31 09:36:29 +0800

    重构通用工具 tool.js

--- utility/tool.js
+++ utility/tool.js
@@ -6,22 +6,27 @@ var fs = require('fs');
  * @param conditions 查询条件，如 {"name":"value"}
  * @returns {Object} 匹配的JSON对象
  */
-exports.jsonQuery = function (jsonArray, conditions) {
+function jsonQuery(jsonArray, conditions)
+{
     var i = 0,
         len = jsonArray.length,
         json,
         condition,
         flag;
-    for (; i < len; i++) {
+    for (; i < len; i++)
+    {
         flag = true;
         json = jsonArray[i];
-        for (condition in conditions) {
-            if (json[condition] !== conditions[condition]) {
+        for (condition in conditions)//遍历条件
+        {
+            if (json[condition] !== conditions[condition])//该条件不被符合，直接退出
+            {
                 flag = false;
                 break;
             }
         }
-        if (flag) {
+        if (flag)
+        {
             return json;
         }
     }
@@ -33,18 +38,23 @@ exports.jsonQuery = function (jsonArray, conditions) {
  * @param [key] 要读取的配置项key
  * @param callback 回调函数
  */
-exports.getConfig = function (filePath, key, callback) {
-    if(typeof key === 'function'){
+function getConfig(filePath, key, callback)
+{
+    if (typeof key === 'function')
+    {
         callback = key;
         key = undefined;
     }
-    fs.readFile(filePath, 'utf8', function (err, file) {
-        if (err) {
+    fs.readFile(filePath, 'utf8', function (err, file)
+    {
+        if (err)
+        {
             console.log('读取文件%s出错：' + err, filePath);
             return callback(err);
         }
         var data = JSON.parse(file);
-        if (typeof key === 'string') {
+        if (typeof key === 'string')
+        {
             data = data[key];
         }
         return callback(null, data);
@@ -56,11 +66,14 @@ exports.getConfig = function (filePath, key, callback) {
  * @param filePath 文件路径
  * @param setters 要写入的对象
  */
-exports.setConfig = function (filePath, setters) {
-    fs.readFile(filePath, 'utf8', function (err, file) {
+function setConfig(filePath, setters)
+{
+    fs.readFile(filePath, 'utf8', function (err, file)
+    {
         var data = JSON.parse(file),
             key;
-        for (key in setters) {
+        for (key in setters)
+        {
             data[key] = setters[key];
         }
         var newFile = JSON.stringify(data, null, 2);
@@ -74,25 +87,35 @@ exports.setConfig = function (filePath, setters) {
  * @param obj 待解析对象
  * @returns {string} 拼装的key，带前缀的形如：prefix_name_Tom_age_20，不带前缀的形如：name_Tom_age_20
  */
-exports.generateKey = function (prefix, obj) {
-    if (typeof prefix === 'object') {
+function generateKey(prefix, obj)
+{
+    if (typeof prefix === 'object')
+    {
         obj = prefix;
         prefix = undefined;
     }
     var attr,
         value,
         key = '';
-    for (attr in obj) {
+    for (attr in obj)
+    {
         value = obj[attr];
         //形如： _name_Tom
         key += '_' + attr.toString().toLowerCase() + '_' + value.toString()
     }
-    if (prefix) {
+    if (prefix)
+    {
         //形如：prefix_name_Tom_age_20
         key = prefix + key;
-    } else {
+    } else
+    {
         //形如：name_Tom_age_20
         key = key.substr(1);
     }
     return key;
 };
+
+exports.jsonQuery = jsonQuery;  //搜索JSON数组
+exports.getConfig = getConfig;  //读取配置文件
+exports.setConfig =  setConfig; //写入配置文件
+exports.generateKey =  generateKey; //根据对象的属性和值拼装key
